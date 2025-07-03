a8c53948d832fa1876a2a6edfccbf7b02947288f
ArshathAhamed <arshathda00@gmail.com>
2025-07-03 14:27:50 +0530

    bug-fixed

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -17,7 +17,7 @@ const HeroSection = () => {
                 </p>
 
                 <div className='pt-4 opacity-0 animate-fade-in-delay-4'>
-                  <a href="#project" className='cosmic-button'>
+                  <a href="#projects" className='cosmic-button'>
                     View My Work
                   </a>
                 </div>
